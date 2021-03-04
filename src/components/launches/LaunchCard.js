d94bc2a6139ba4f1b52b4c51ad40b627ba3fe1fa
Pranit Maske <maskepranit@gmail.com>
2021-03-04 22:43:38 +0530

    - Changed to Class based component

--- src/components/launches/LaunchCard.js
+++ src/components/launches/LaunchCard.js
@@ -1,51 +1,62 @@
 import React from "react";
 import LaunchModal from "./LaunchModal";
 import { getLaunch } from "../../redux/actions/launchActions";
-import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 
-const LaunchCard = ({ cardTitle, cardSubTitle, cardText, flightId }) => {
-  const launchDate = new Date(cardSubTitle);
+class LaunchCard extends React.Component {
+  launchDate = new Date(this.props.cardSubTitle);
 
-  const launchData = useSelector((state) => state.launches.launch);
-
-  const dispatch = useDispatch();
-
-  const handleClick = () => {
-    dispatch(getLaunch(flightId));
+  handleClick = async (id) => {
+    await getLaunch(id);
   };
 
-  return (
-    <div className="container">
-      <div className="card border-0">
-        <div className="card-body">
-          <h5 className="card-title">{cardTitle}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">
-            {launchDate.toLocaleString()}
-          </h6>
-          <p className="card-text">{cardText}</p>
-          <button
-            type="button"
-            className="btn btn-link btn-sm"
-            style={{ padding: 0 }}
-            data-toggle="modal"
-            data-target="#launchModal"
-            onClick={handleClick}
-          >
-            Learn More
-          </button>
-          <div
-            className="modal fade"
-            id="launchModal"
-            tabIndex="-1"
-            aria-labelledby="launchModal"
-            aria-hidden="true"
-          >
-            <LaunchModal flightData={launchData} />
+  render() {
+    const { cardTitle, launchDate, flightId, launchData, cardText } = this.props && this.props;
+    return (
+      <div className="container">
+        <div className="card border-0">
+          <div className="card-body">
+            <h5 className="card-title">{cardTitle}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              { launchDate && launchDate.toLocaleString()}
+            </h6>
+            <p className="card-text">{cardText}</p>
+            <button
+              type="button"
+              className="btn btn-link btn-sm"
+              style={{ padding: 0 }}
+              data-toggle="modal"
+              data-target="#launchModal"
+              onClick={() => this.handleClick(flightId)}
+            >
+              Learn More
+            </button>
+            <div
+              className="modal fade"
+              id="launchModal"
+              tabIndex="-1"
+              aria-labelledby="launchModal"
+              aria-hidden="true"
+            >
+              <LaunchModal flightData={launchData} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    launchData: state.launches.launch,
+  };
 };
 
-export default LaunchCard;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getLaunch: async () => await dispatch(getLaunch()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LaunchCard);
